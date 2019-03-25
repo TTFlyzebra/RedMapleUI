0632ba8aff5d8a5633eb7c6df56b159527e7edf3
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-03-25 18:04:23 +0800

    1.ren example name to app

--- flyuilib/src/main/java/com/flyzebra/flyui/view/pageanimtor/PageTransformerPage.java
+++ flyuilib/src/main/java/com/flyzebra/flyui/view/pageanimtor/PageTransformerPage.java
@@ -14,17 +14,21 @@ public class PageTransformerPage implements ViewPager.PageTransformer {
     public void transformPage(View view, float position) {
         int pagerWidth = view.getWidth();
 
-        if (position >= 1 || position <= -1) {
-            view.setVisibility(View.GONE);
-        } else {
+        if (position > -1.0f && position < 1.0f) {
             view.setVisibility(View.VISIBLE);
+        } else {
+            view.setVisibility(View.INVISIBLE);
         }
 
         if (position >= 0) {
             float translationX = (0 - pagerWidth) * position;
             view.setTranslationX(translationX);
-            view.setScaleX(1-position);
-            view.setScaleY(1-position);
+            view.setScaleX(1 - position);
+            view.setScaleY(1 - position);
+        } else {
+            view.setTranslationX(0);
+            view.setScaleX(1);
+            view.setScaleY(1);
         }
         if (position > -1 && position < 0) {
             view.setAlpha((position * position * position + 1));
