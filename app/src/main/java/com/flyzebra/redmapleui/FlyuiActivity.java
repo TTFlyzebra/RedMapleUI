77f5c52efc1c706d7e013085393a3931eb6abac5
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-03-28 18:04:43 +0800

    upversion topbean ok

--- app/src/main/java/com/flyzebra/redmapleui/FlyuiActivity.java
+++ app/src/main/java/com/flyzebra/redmapleui/FlyuiActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.flyzebra.flyui.FlyuiAction;
 import com.flyzebra.flyui.bean.Action;
 import com.flyzebra.flyui.bean.ThemeBean;
@@ -22,7 +26,7 @@ import com.flyzebra.flyui.view.themeview.ThemeView;
  * 2019/3/20 10:55
  * Describ:
  **/
-public class FlyuiActivity extends Activity implements FlyuiAction,IUpdataVersion.CheckCacheResult, IUpdataVersion.UpResult {
+public class FlyuiActivity extends Activity implements FlyuiAction, IUpdataVersion.CheckCacheResult, IUpdataVersion.UpResult {
 
     private ThemeView mThemeView;
     public IUpdataVersion iUpDataVersion;
@@ -36,7 +40,7 @@ public class FlyuiActivity extends Activity implements FlyuiAction,IUpdataVersio
         setContentView(mThemeView);
         mThemeView.onCreate(this);
         iDiskCache = new DiskCache().init(this);
-        iUpDataVersion = new UpdataVersion(getApplicationContext(), iDiskCache){
+        iUpDataVersion = new UpdataVersion(getApplicationContext(), iDiskCache) {
             @Override
             public void initApi() {
                 String url = SysproUtils.get(FlyuiActivity.this, SysproUtils.Property.URL_BASE, "http://192.168.1.119:801/uiweb");
@@ -75,7 +79,7 @@ public class FlyuiActivity extends Activity implements FlyuiAction,IUpdataVersio
 
     @Override
     public void upVersionOK(ThemeBean themeBean) {
-        mThemeView.upData(themeBean);
+        upView(themeBean);
     }
 
     @Override
@@ -90,12 +94,26 @@ public class FlyuiActivity extends Activity implements FlyuiAction,IUpdataVersio
 
     @Override
     public void getCacheDataOK(ThemeBean themeBean) {
-        mThemeView.upData(themeBean);
+        upView(themeBean);
         iUpDataVersion.forceUpVersion(this);
     }
 
     @Override
     public void getCacheDataFaile(String error) {
         iUpDataVersion.forceUpVersion(this);
+    }
+
+    private void upView(ThemeBean themeBean) {
+        mThemeView.upData(themeBean);
+        if (TextUtils.isEmpty(themeBean.imageurl)) {
+           getWindow().getDecorView().setBackground(null);
+        } else {
+            Glide.with(this).load(themeBean.imageurl).override(1024,600).into(new SimpleTarget<GlideDrawable>() {
+                @Override
+                public void onResourceReady(GlideDrawable glideDrawable, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                    getWindow().getDecorView().setBackground(glideDrawable);
+                }
+            });
+        }
     }
 }
